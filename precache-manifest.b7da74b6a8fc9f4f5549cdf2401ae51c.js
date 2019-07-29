551e63bda08f623b3996e10265b56dbb7c89e895
SergijPSO <pankivsergij97@gmail.com>
2019-07-29 22:23:54 +0300

    Updates

---
+++ precache-manifest.b7da74b6a8fc9f4f5549cdf2401ae51c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0aca26cc6887d9d297762772e233f8e",
+    "url": "/reactpage/index.html"
+  },
+  {
+    "revision": "16af771cfd3562ace0fc",
+    "url": "/reactpage/static/css/main.b89092d3.chunk.css"
+  },
+  {
+    "revision": "dbf8e2aa25df7140abbe",
+    "url": "/reactpage/static/js/2.2d9f984d.chunk.js"
+  },
+  {
+    "revision": "16af771cfd3562ace0fc",
+    "url": "/reactpage/static/js/main.e8a01da5.chunk.js"
+  },
+  {
+    "revision": "bbc2b6450ac1e8e7301d",
+    "url": "/reactpage/static/js/runtime~main.effe8ecf.js"
+  }
+]);
